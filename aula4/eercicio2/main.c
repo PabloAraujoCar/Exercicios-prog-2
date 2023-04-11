3a12913115eb23b18a5f61ba3658e18f0b8c95c5
Pablo Araujo <111598631+PabloAraujoCar@users.noreply.github.com>
2023-04-11 18:30:16 -0300

    Create main.c

---
+++ aula4/eercicio2/main.c
@@ -0,0 +1,7 @@
+Criar uma biblioteca com capacidade máxima de 1000 livros
+Adicionar livros à biblioteca
+Remover livros da biblioteca de acordo com título ou identificador único
+Imprimir todos os livros da biblioteca
+Buscar e imprimir um livro pelo título
+Emprestar livro pelo título
+Devolver um livro pelo título
